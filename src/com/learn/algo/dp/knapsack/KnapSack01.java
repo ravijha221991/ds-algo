b416e1ab2fbe821f0afcd022bf5c0f73f528173c
Ravindra jha <ravindra.jha@in.g4s.com>
2020-07-15 19:21:04 +0530

    dp:01Knapsack:memoized&topdown approach

--- src/com/learn/algo/dp/knapsack/KnapSack01.java
+++ src/com/learn/algo/dp/knapsack/KnapSack01.java
@@ -7,8 +7,31 @@ public class KnapSack01 {
 		int[] profits = new int[]{10,50,30,70,90,20};
 		int W = 12;
 		int n = weights.length;
+		System.out.println("*****Recursive Test************");
 		System.out.println(recursive(weights,profits,W,n));
+		
+		System.out.println("*****Memoized Test************");
+		int dp[][] = new int[101][1001];
+		for(int i=0;i<101;i++) {
+			for(int j=0;j<1001;j++) {
+				dp[i][j]=-1;
+			}
+		}
+		
+		System.out.println(memoized(weights,profits,W,n,dp));
+		
+		System.out.println("*****Top Down Test************");
+		 dp = new int[n+1][W+1];
+		for(int i=0;i<n+1;i++) {
+			dp[i][0]=0;
+		}
+		for(int j=0;j<W+1;j++) {
+			dp[0][j]=0;
+		}
+		System.out.println(topdown(weights,profits,W,n,dp));
+		
 	}
+
 
 	private static int recursive(int[] weights, int[] profits, int W, int n) {
 		if(W==0||n==0) {
@@ -20,6 +43,35 @@ public class KnapSack01 {
 		}else {
 			return recursive(weights,profits,W,n-1);
 		}
+	}
+	
+	private static int memoized(int[] weights,int[] profits,int W,int n,int[][] dp) {
+		if(W==0||n==0) {
+			return 0;
+		}
+		if(dp[n][W] != -1) {
+			return dp[n][W];
+		}
+		if(weights[n-1]<=W) {
+			dp[n][W]= Math.max(profits[n-1]+recursive(weights,profits,W-weights[n-1],n-1), recursive(weights,profits,W,n-1));
+		}else {
+			dp[n][W]=recursive(weights,profits,W,n-1);
+		}
+		
+		return dp[n][W];
+	}
+	
+	private static int topdown(int[] weights, int[] profits, int W, int n, int[][] dp) {
+		for(int i=1;i<n+1;i++) {
+			for(int j=1;j<W+1;j++) {
+				if(weights[i-1]<=j) {
+					dp[i][j]=Math.max(profits[i-1]+dp[i-1][j-weights[i-1]], dp[i-1][j]);
+				}else {
+					dp[i][j]=dp[i-1][j];
+				}
+			}
+		}
+		return dp[n][W];
 	}
 
 }
